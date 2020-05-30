6c0d26da64501158bac049792e87e699aee0a56f
Lukas Dörig <lukee910@gmail.com>
2020-05-30 17:05:54 +0200

    Example ex 1

---
+++ M318.Excercises/M318.Excercises.Nr1/ListForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace M318.Exercises.Nr1
+{
+    public partial class ListForm : Form
+    {
+        public ListForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            var text = $"{lbxTexts.Items.Count + 1}: {txbText.Text}";
+            lbxTexts.Items.Add(text);
+            txbText.Text = "";
+        }
+
+        private void txbText_TextChanged(object sender, EventArgs e)
+        {
+            btnAdd.Enabled = txbText.Text.Length > 0;
+        }
+    }
+}
