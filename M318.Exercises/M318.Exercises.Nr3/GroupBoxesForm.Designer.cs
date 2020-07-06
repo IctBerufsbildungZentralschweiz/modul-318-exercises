44f92b2bcc38e05b9ea7ec1383aa8073758971d6
Lukas Dörig <lukee910@gmail.com>
2020-07-06 10:41:04 +0200

    Add Accept Button

--- M318.Exercises/M318.Exercises.Nr3/GroupBoxesForm.Designer.cs
+++ M318.Exercises/M318.Exercises.Nr3/GroupBoxesForm.Designer.cs
@@ -47,7 +47,7 @@
             this.checkBoxesGroup.SuspendLayout();
             this.SuspendLayout();
             // 
-            // gbxRadioButtons1
+            // radioButtonsBox1
             // 
             this.radioButtonsBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
@@ -55,46 +55,46 @@
             this.radioButtonsBox1.Controls.Add(this.radioButtonOption12);
             this.radioButtonsBox1.Controls.Add(this.radioButtonOption11);
             this.radioButtonsBox1.Location = new System.Drawing.Point(12, 12);
-            this.radioButtonsBox1.Name = "gbxRadioButtons1";
+            this.radioButtonsBox1.Name = "radioButtonsBox1";
             this.radioButtonsBox1.Size = new System.Drawing.Size(331, 51);
             this.radioButtonsBox1.TabIndex = 0;
             this.radioButtonsBox1.TabStop = false;
             this.radioButtonsBox1.Text = "Group 1";
             // 
-            // rbtOption13
+            // radioButtonOption13
             // 
             this.radioButtonOption13.AutoSize = true;
             this.radioButtonOption13.Location = new System.Drawing.Point(166, 19);
-            this.radioButtonOption13.Name = "rbtOption13";
+            this.radioButtonOption13.Name = "radioButtonOption13";
             this.radioButtonOption13.Size = new System.Drawing.Size(74, 17);
             this.radioButtonOption13.TabIndex = 2;
             this.radioButtonOption13.TabStop = true;
             this.radioButtonOption13.Text = "Option 1.3";
             this.radioButtonOption13.UseVisualStyleBackColor = true;
             // 
-            // rbtOption12
+            // radioButtonOption12
             // 
             this.radioButtonOption12.AutoSize = true;
             this.radioButtonOption12.Location = new System.Drawing.Point(86, 19);
-            this.radioButtonOption12.Name = "rbtOption12";
+            this.radioButtonOption12.Name = "radioButtonOption12";
             this.radioButtonOption12.Size = new System.Drawing.Size(74, 17);
             this.radioButtonOption12.TabIndex = 1;
             this.radioButtonOption12.TabStop = true;
             this.radioButtonOption12.Text = "Option 1.2";
             this.radioButtonOption12.UseVisualStyleBackColor = true;
             // 
-            // rbtOption11
+            // radioButtonOption11
             // 
             this.radioButtonOption11.AutoSize = true;
             this.radioButtonOption11.Location = new System.Drawing.Point(6, 19);
-            this.radioButtonOption11.Name = "rbtOption11";
+            this.radioButtonOption11.Name = "radioButtonOption11";
             this.radioButtonOption11.Size = new System.Drawing.Size(74, 17);
             this.radioButtonOption11.TabIndex = 0;
             this.radioButtonOption11.TabStop = true;
             this.radioButtonOption11.Text = "Option 1.1";
             this.radioButtonOption11.UseVisualStyleBackColor = true;
             // 
-            // gbxRadioButtons2
+            // radioButtonsBox2
             // 
             this.radioButtonsBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
@@ -102,46 +102,46 @@
             this.radioButtonsBox2.Controls.Add(this.radioButtonOption21);
             this.radioButtonsBox2.Controls.Add(this.radioButtonOption22);
             this.radioButtonsBox2.Location = new System.Drawing.Point(12, 69);
-            this.radioButtonsBox2.Name = "gbxRadioButtons2";
+            this.radioButtonsBox2.Name = "radioButtonsBox2";
             this.radioButtonsBox2.Size = new System.Drawing.Size(331, 51);
             this.radioButtonsBox2.TabIndex = 3;
             this.radioButtonsBox2.TabStop = false;
             this.radioButtonsBox2.Text = "Group 2";
             // 
-            // rbtOption23
+            // radioButtonOption23
             // 
             this.radioButtonOption23.AutoSize = true;
             this.radioButtonOption23.Location = new System.Drawing.Point(166, 19);
-            this.radioButtonOption23.Name = "rbtOption23";
+            this.radioButtonOption23.Name = "radioButtonOption23";
             this.radioButtonOption23.Size = new System.Drawing.Size(74, 17);
             this.radioButtonOption23.TabIndex = 5;
             this.radioButtonOption23.TabStop = true;
             this.radioButtonOption23.Text = "Option 2.3";
             this.radioButtonOption23.UseVisualStyleBackColor = true;
             // 
-            // rbtOption22
+            // radioButtonOption21
             // 
             this.radioButtonOption21.AutoSize = true;
             this.radioButtonOption21.Location = new System.Drawing.Point(86, 19);
-            this.radioButtonOption21.Name = "rbtOption22";
+            this.radioButtonOption21.Name = "radioButtonOption21";
             this.radioButtonOption21.Size = new System.Drawing.Size(74, 17);
             this.radioButtonOption21.TabIndex = 4;
             this.radioButtonOption21.TabStop = true;
             this.radioButtonOption21.Text = "Option 2.2";
             this.radioButtonOption21.UseVisualStyleBackColor = true;
             // 
-            // rbtOption21
+            // radioButtonOption22
             // 
             this.radioButtonOption22.AutoSize = true;
             this.radioButtonOption22.Location = new System.Drawing.Point(6, 19);
-            this.radioButtonOption22.Name = "rbtOption21";
+            this.radioButtonOption22.Name = "radioButtonOption22";
             this.radioButtonOption22.Size = new System.Drawing.Size(74, 17);
             this.radioButtonOption22.TabIndex = 3;
             this.radioButtonOption22.TabStop = true;
             this.radioButtonOption22.Text = "Option 2.1";
             this.radioButtonOption22.UseVisualStyleBackColor = true;
             // 
-            // gbxCheckBoxes
+            // checkBoxesGroup
             // 
             this.checkBoxesGroup.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
@@ -149,58 +149,58 @@
             this.checkBoxesGroup.Controls.Add(this.secondCheckBox);
             this.checkBoxesGroup.Controls.Add(this.firstCheckBox);
             this.checkBoxesGroup.Location = new System.Drawing.Point(12, 126);
-            this.checkBoxesGroup.Name = "gbxCheckBoxes";
+            this.checkBoxesGroup.Name = "checkBoxesGroup";
             this.checkBoxesGroup.Size = new System.Drawing.Size(331, 51);
             this.checkBoxesGroup.TabIndex = 4;
             this.checkBoxesGroup.TabStop = false;
             this.checkBoxesGroup.Text = "Checkboxes";
             // 
-            // chbThird
+            // thirdCheckBox
             // 
             this.thirdCheckBox.AutoSize = true;
             this.thirdCheckBox.Location = new System.Drawing.Point(166, 19);
-            this.thirdCheckBox.Name = "chbThird";
+            this.thirdCheckBox.Name = "thirdCheckBox";
             this.thirdCheckBox.Size = new System.Drawing.Size(50, 17);
             this.thirdCheckBox.TabIndex = 8;
             this.thirdCheckBox.Text = "Third";
             this.thirdCheckBox.UseVisualStyleBackColor = true;
             // 
-            // chbSecond
+            // secondCheckBox
             // 
             this.secondCheckBox.AutoSize = true;
             this.secondCheckBox.Location = new System.Drawing.Point(86, 19);
-            this.secondCheckBox.Name = "chbSecond";
+            this.secondCheckBox.Name = "secondCheckBox";
             this.secondCheckBox.Size = new System.Drawing.Size(63, 17);
             this.secondCheckBox.TabIndex = 7;
             this.secondCheckBox.Text = "Second";
             this.secondCheckBox.UseVisualStyleBackColor = true;
             // 
-            // chbFirst
+            // firstCheckBox
             // 
             this.firstCheckBox.AutoSize = true;
             this.firstCheckBox.Location = new System.Drawing.Point(6, 19);
-            this.firstCheckBox.Name = "chbFirst";
+            this.firstCheckBox.Name = "firstCheckBox";
             this.firstCheckBox.Size = new System.Drawing.Size(45, 17);
             this.firstCheckBox.TabIndex = 6;
             this.firstCheckBox.Text = "First";
             this.firstCheckBox.UseVisualStyleBackColor = true;
             // 
-            // btnShowResultAdvanced
+            // showResultsAdvancedButton
             // 
             this.showResultsAdvancedButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.showResultsAdvancedButton.Location = new System.Drawing.Point(210, 183);
-            this.showResultsAdvancedButton.Name = "btnShowResultAdvanced";
+            this.showResultsAdvancedButton.Name = "showResultsAdvancedButton";
             this.showResultsAdvancedButton.Size = new System.Drawing.Size(133, 23);
             this.showResultsAdvancedButton.TabIndex = 7;
             this.showResultsAdvancedButton.Text = "Show Result (Improved)";
             this.showResultsAdvancedButton.UseVisualStyleBackColor = true;
             this.showResultsAdvancedButton.Click += new System.EventHandler(this.ShowResultAdvancedClicked);
             // 
-            // btnShowResultSimple
+            // showResultsSimpleButton
             // 
             this.showResultsSimpleButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.showResultsSimpleButton.Location = new System.Drawing.Point(54, 183);
-            this.showResultsSimpleButton.Name = "btnShowResultSimple";
+            this.showResultsSimpleButton.Name = "showResultsSimpleButton";
             this.showResultsSimpleButton.Size = new System.Drawing.Size(150, 23);
             this.showResultsSimpleButton.TabIndex = 6;
             this.showResultsSimpleButton.Text = "Show Result (Redundant)";
@@ -209,6 +209,7 @@
             // 
             // GroupBoxesForm
             // 
+            this.AcceptButton = this.showResultsAdvancedButton;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(355, 214);
